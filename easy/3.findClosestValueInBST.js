347c669271a30a58b262f6a13c3303c8e4a3d472
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-10-01 21:50:14 -0500

    feat: add find closest value on BST

---
+++ easy/3.findClosestValueInBST.js
@@ -0,0 +1,63 @@
+const findClosestValueInBst = (tree, target) => {
+  return findClosestValue(tree, target, tree.value)
+}
+
+const findClosestValue = (tree, target, closest) => {
+  if (tree === null) return closest
+
+  if (Math.abs(target - tree.value) < closest) {
+    closest = tree.value
+  }
+
+  if (target > tree.value) {
+    return findClosestValue(tree.right, target, closest)
+  }
+
+  if (target < tree.value) {
+    return findClosestValue(tree.left, target, closest)
+  }
+
+  return tree.value
+}
+
+// This is the class of the input tree. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+const caseOne = {
+  "tree": {
+    "nodes": [
+      {"id": "10", "left": "5", "right": "15", "value": 10},
+      {"id": "15", "left": "13", "right": "22", "value": 15},
+      {"id": "22", "left": null, "right": null, "value": 22},
+      {"id": "13", "left": null, "right": "14", "value": 13},
+      {"id": "14", "left": null, "right": null, "value": 14},
+      {"id": "5", "left": "2", "right": "5-2", "value": 5},
+      {"id": "5-2", "left": null, "right": null, "value": 5},
+      {"id": "2", "left": "1", "right": null, "value": 2},
+      {"id": "1", "left": null, "right": null, "value": 1}
+    ],
+    "root": "10"
+  },
+  "target": 12
+}
+
+const root = new BST(10);
+root.left = new BST(5);
+root.left.left = new BST(2);
+root.left.left.left = new BST(1);
+root.left.right = new BST(5);
+root.right = new BST(15);
+root.right.left = new BST(13);
+root.right.left.right = new BST(14);
+root.right.right = new BST(22);
+
+const expected = 13;
+
+console.log(findClosestValueInBst(root, 12))
