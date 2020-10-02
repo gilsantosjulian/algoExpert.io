e1db265831771b047a441b9e7fb9d79422100d87
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-10-01 19:45:41 -0500

    feat: refactor and add one example

--- easy/2.validateSubsequence.js
+++ easy/2.validateSubsequence.js
@@ -1,4 +1,5 @@
-function isValidSubsequence(array, sequence) {
+// O(n) time for the main array | O(1) just a couple of indexes
+const isValidSubsequence = (array, sequence) => {
 	let arrIdx = 0
 	let seqIdx = 0
 	
@@ -8,3 +9,8 @@ function isValidSubsequence(array, sequence) {
 	}
 	return seqIdx === sequence.length
 }
+
+const array = [5, 1, 22, 25, 6, -1, 8, 10]
+const subsequence = [1, 6, -1, 10]
+
+console.log(isValidSubsequence(array, subsequence))
